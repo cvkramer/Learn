aaefe381e30b70e7c9822c0efbacdffb1b768ff2
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2016-02-09 14:12:53 -0500

    basic data fetch

--- data.js
+++ data.js
@@ -1,12 +1,18 @@
-window.onload = function(e) {
-    var rootDiv = document.getElementById('root');
+window.addEventListener('load', function(event) {
+    console.log('onload');
     $.ajax({
-        url: '<data url goes here>',
+        url: 'http://localhost:8000/data.JSON',
+        crossDomain: true,
         method: 'get',
         success: loadData
     });
-}
 
-function loadData(json) {
-    // read the json blob and do stuff
-}
+    function loadData(json) {
+        // read the json blob and do stuff
+        console.log('data is loaded!');
+        console.log(json);
+        //var rootDiv = document.getElementById('root');
+    }
+});
+
+
